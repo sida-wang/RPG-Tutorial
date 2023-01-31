078ee8d1c71fc0b5e8915090b770ae5f5f02375d
Sida Wang <42928828+sida-wang@users.noreply.github.com>
2023-01-31 20:05:31 +1100

    Created player model and game session viewmodel In line with lesson 3.4

---
+++ Engine/Models/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Models.ViewModels
+{
+    internal class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+        
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Sida";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
