feca250f073d9911f41ea3e4b8ccb045dca5ff8b
Sida Wang <42928828+sida-wang@users.noreply.github.com>
2023-02-01 18:08:13 +1100

    Created location class - ignoring disappearing images atm due to incompatibility with .NET 6 In line with lesson 4.1

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Engine.Models;
+﻿using Engine.Models;
 
 namespace Engine.ViewModels
 {
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
 
         public GameSession()
         {
@@ -21,6 +17,15 @@ namespace Engine.ViewModels
                 HitPoints = 10,
                 ExperiencePoints = 0,
                 Level = 1,
+            };
+
+            CurrentLocation = new Location
+            {
+                Name = "Home",
+                XCoordinate = 0,
+                YCoordinate = -1,
+                Description = "This is your house",
+                ImageName = "/Engine;component/Images/Locations/Home.png",
             };
         }
     }
