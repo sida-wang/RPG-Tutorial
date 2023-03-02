0ed11c20bd4cb49bf8f1c2497ced5d0990235386
Sida Wang <42928828+sida-wang@users.noreply.github.com>
2023-03-02 21:13:06 +1100

    Adding Game Items and Weapons with ItemFactory static class In line with Lesson 5.1

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,26 @@
+﻿using Engine.Models;
+
+namespace Engine.Factories
+{
+    internal static class ItemFactory
+    {
+        private static List<GameItem> _standardGameItems;
+        static ItemFactory()
+        {
+            _standardGameItems = new List<GameItem>
+            {
+                new Weapon(1001, "Pointy Stick", 1, 1, 2),
+                new Weapon(1002, "Rusty Sword", 5, 1, 3)
+            };
+        }
+        public static GameItem CreateGameItem(int itemTypeID)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
+            if (standardItem != null)
+            {
+                return standardItem.Clone();
+            }
+            return null;
+        }
+    }
+}
